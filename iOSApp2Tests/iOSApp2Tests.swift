76543d23792b2df39a5b5cafbd410a4ac4492e96
Abdul <abdulaziz_safi@yahoo.com>
2025-10-01 23:34:32 -0400

    Initial Commit

---
+++ iOSApp2Tests/iOSApp2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp2Tests.swift
+//  iOSApp2Tests
+//
+//  Created by Abdul Aziz Safai on 2025-10-01.
+//
+
+import Testing
+@testable import iOSApp2
+
+struct iOSApp2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
